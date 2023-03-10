c8764dfe14cf7fe04a2af720316878b8179f70d1
mhmdawnallah <mohamedmohey2352@gmail.com>
2023-03-10 18:54:03 +0200

    Update newtrainer

--- src/newtrainer.py
+++ src/newtrainer.py
@@ -1,6 +1,5 @@
 from typing import List
 from abc import ABC
-import numpy as np
 
 class NewTrainer(ABC):
 	def train(self, x: List[List[float]], y: List[float]):
@@ -9,32 +8,3 @@ class NewTrainer(ABC):
 	def predict(self, x: List[float]) -> float:
 		pass
 
-class LinearRegression(NewTrainer):
-    def __init__(self):
-        self.coef_ = None
-
-    def train(self, x: List[List[float]], y: List[float]):
-        if len(x) != len(y):
-            raise ValueError("Input arrays must have the same length")
-        if len(x) == 0 or len(y) == 0:
-            raise ValueError("Input arrays must not be empty")
-
-        # Add a column of ones to x for the intercept term
-        x = np.insert(x, 0, 1, axis=1)
-
-        # Compute the coefficients using the normal equation
-        x_transpose = x.T
-        x_transpose_x = np.dot(x_transpose, x)
-        x_transpose_x_inv = np.linalg.inv(x_transpose_x)
-        x_transpose_y = np.dot(x_transpose, y)
-        self.coef_ = np.dot(x_transpose_x_inv, x_transpose_y)
-
-    def predict(self, x: List[float]) -> float:
-        if self.coef_ is None:
-            raise ValueError("Model has not been trained yet")
-        if len(x) != len(self.coef_) - 1:
-            raise ValueError("Input length does not match coefficient length")
-
-        # Add a 1 to the beginning of x for the intercept term
-        x_with_intercept = [1] + x
-        return np.dot(self.coef_, x_with_intercept)
