f629709b25780fa21e322af96f20a6b144d22ffe
mhmdawnallah <mohamedmohey2352@gmail.com>
2023-03-13 19:02:30 +0200

    Add test_train_convergence

--- tests/test_linear_regression.py
+++ tests/test_linear_regression.py
@@ -19,6 +19,14 @@ def test_train(linear_regression_model):
     linear_regression_model.train(x, y)
     assert np.allclose(linear_regression_model.coef_, [0, 2])
 
+def test_train_convergence(linear_regression_model):
+    x = [[1], [2], [3], [4], [5]]
+    y = [2, 4, 6, 8, 10]
+    linear_regression_model.train(x, y)
+    expected_coef = [0, 2]
+    actual_coef = linear_regression_model.coef_
+    assert np.allclose(actual_coef, expected_coef, rtol=1e-6, atol=1e-6)
+	
 def test_train_empty_input(linear_regression_model):
     x = []
     y = []
